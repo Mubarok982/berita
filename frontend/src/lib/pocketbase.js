db40033af2340e7305c63a605ebaee749a8be624
Mubarok982 <rizqymubarok99@gmail.com>
2026-02-08 23:28:04 +0700

    update url

--- frontend/src/lib/pocketbase.js
+++ frontend/src/lib/pocketbase.js
@@ -1,7 +1,3 @@
 import PocketBase from 'pocketbase';
 
-export const pb = new PocketBase('https://web-mapala-backend.hf.space');
-
-// Optionally, you can enable debug mode for PocketBase
-// pb.autoCancellation(false); // Disable auto cancellation of requests
-// pb.debug = true; // Enable debug logging
+export const pb = new PocketBase('https://rizqy123-web-mapala-backend.hf.space');
